89521ea08731e697e0582286ac6f86a3e204b708
yankiweiss <yakovw2706@gmail.com>
2025-01-22 09:55:22 -0500

    update

--- api/index.js
+++ api/index.js
@@ -3,11 +3,36 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3501;
 const cors = require("cors");
+const supabase = require('./supabase');
+const { error } = require("console");
 
 
-
+app.use(express.json());
 
 app.use(express.static('public'));
+
+app.post('/newpatient', async(req, res) => {
+  const {name , date_of_service} = req.body 
+
+  if(!name || !date_of_service) {
+    return res.status(400).json({error: 'Name and Date of Service are required.'})
+  }
+
+  try {
+    const {data , error} = await supabase
+    .from('patients')
+    .insert([{name , date_of_service}])
+
+  if (error){
+    throw error
+  }
+
+  res.status(201).json({message: 'patient added Succsesfuly ', data});
+
+}catch (error){
+res.status(500).json({error: error.message});
+}
+})
 
 
 app.get("/", (req, res) => {
---
+++ client/create.js
@@ -0,0 +1,27 @@
+document.getElementById('userForm').addEventListener('submit', async function (event) {
+    event.preventDefault(); // Prevent the form from refreshing the page
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+  
+    try {
+      const response = await fetch('https://best-care.vercel.app/newpatient', { // Replace with your backend URL
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email }), // Send name and email as JSON
+      });
+  
+      const data = await response.json();
+  
+      if (response.ok) {
+        document.getElementById('responseMessage').innerText = `Success: ${data.message}`;
+      } else {
+        document.getElementById('responseMessage').innerText = `Error: ${data.error}`;
+      }
+    } catch (error) {
+      document.getElementById('responseMessage').innerText = `Error: ${error.message}`;
+    }
+  });
+  
